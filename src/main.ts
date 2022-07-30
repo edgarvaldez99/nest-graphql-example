789d748e4ddbc0df361d870484ed09c5ee2d6c7b
evaldez <edgarvaldez99@gmail.com>
2022-07-30 01:55:09 -0400

    added class-transformer dependency

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      validationError: {
+        target: false,
+      },
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
