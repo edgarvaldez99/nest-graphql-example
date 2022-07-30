0a1c9beb39124b1bfffd9ad033c273ea83b806c7
evaldez <edgarvaldez99@gmail.com>
2022-07-29 23:41:59 -0400

    added task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigurationModule } from './config/configuration.module';
+import { DatabaseModule } from './database/database.module';
+import { GraphqlModule } from './graphql/graphql.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigurationModule, DatabaseModule, GraphqlModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
